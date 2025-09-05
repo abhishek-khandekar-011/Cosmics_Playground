322a176e31fd34c715b07efe76bd1eb5e7ba7d22
Abhishek Khandekar <khandekarabhishek11@gmail.com>
2025-09-05 22:15:16 +0530

    Update 1.Half_Pyramid.java

--- DSA_Practice/Patterns/1.Half_Pyramid.java
+++ DSA_Practice/Patterns/1.Half_Pyramid.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+public class Half_Pyramid {
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the number of rows:");
+
+        int rows = sc.nextInt();
+
+        for(int i = 0; i < rows + 1 ; i++){
+
+            for(int j = 0; j < i; j++){
+                
+                System.out.print("* ");
+            }
+       
+            System.out.println();
+
+        }
+
+        sc.close();
