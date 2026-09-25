fd509860d8f5ffc5efec44b41b67b50441fd14b5
Abhishek Khandekar <khandekarabhishek11@gmail.com>
2026-09-25 22:12:06 +0530

    Add basic C++ program for summing two numbers

--- C++_Programming/test.cpp
+++ C++_Programming/test.cpp
@@ -1,1 +1,15 @@
 
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to C++ Programming!\n";
+    int num1, num2, sum;
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    sum = num1 + num2;
+    std::cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
+    return 0;
+}
